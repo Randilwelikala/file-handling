c6f1693b47e637708146941b98d3e4fc8144462c
Randilwelikala <125990722+Randilwelikala@users.noreply.github.com>
2024-01-17 12:59:30 +0530

    simple file handling with python

---
+++ demo.py
@@ -0,0 +1,15 @@
+"""file = open("data.txt",'w')
+for i in range(0,100):
+    file.write(str(i)+',')
+
+file.close()"""
+x=['1','2','3','4']
+msg=','.join(x)
+print(msg)
+
+
+file= open("data.txt",'w')
+msg=','.join()
+
+for i in range(0,100):
+    file.write(str(i)+)
